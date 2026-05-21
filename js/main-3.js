09b4c6bd2184e3e4e8e9993f34f72a1ba45041d5
ProPage.in <propagetech@gmail.com>
2026-05-21 19:27:10 +0530

    Update image paths and metadata across multiple HTML files
    Replaced external image URLs with relative paths for improved loading efficiency and consistency across the site. Updated Open Graph and Twitter metadata to reflect the new image paths, ensuring better integration with social media platforms. Adjusted contact address wording for clarity. This change enhances site performance and maintains uniformity in content presentation.

--- js/main-3.js
+++ js/main-3.js
@@ -1252,7 +1252,7 @@ var Viamagus_Website_Loader =  {
 		feedHtml = feedHtml +'<h4 class="vm-twitter-hashtag">'+tweetObj.postedBy+'</h4>';
 		feedHtml = feedHtml +'</div>';
 		feedHtml = feedHtml +'<div class="span2">';
-		feedHtml = feedHtml +'<img class="vm-twitter-bird" src="https://b92520ec2a9525957424-f27d5d7cfb65b8b0cbdd1fc15972b093.ssl.cf1.rackcdn.com/1454927007306twittericon.png">';
+		feedHtml = feedHtml +'<img class="vm-twitter-bird" src="imgs/twitter-icon.png">';
 		feedHtml = feedHtml +'</div>';
 		feedHtml = feedHtml +'</div>';	 
 		
@@ -1957,7 +1957,7 @@ var Viamagus_Website_Loader =  {
 
 	},
     showDirections:function(userLocationLatLng){
-		var mp_marker_url = "https://525d77366d43949fbb11-4ff276156f69726ae3b8de874c89dd19.ssl.cf1.rackcdn.com/1427213263075pointer.png";
+		var mp_marker_url = "imgs/map-pointer.png";
 	    var mp_marker_w = 75;
 	    var mp_marker_h = 103;
 	    var mp_marker_title = "M";
